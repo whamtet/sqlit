6c752c22e620c827984df7a5a160dcc91a118d82
Matthew Molloy <whamtet@gmail.com>
2026-06-27 02:18:54 +0700

    sqlite_wrapper

--- sqlit/domains/connections/providers/sqlite/sqlite_wrapper.py
+++ sqlit/domains/connections/providers/sqlite/sqlite_wrapper.py
@@ -3,17 +3,12 @@ import sqlite3
 import subprocess
 
 ## for dev use
-def spit(text):
+def dump(text):
     with open("dump", "w") as f:
         f.write(text)
 
-def process_rows(sql, cursor):
-
-    sql_regex = r"updateclj\s+(\w+)\s+set\s+(\w+)\s+=(.*?)(where.*?)use primary key(.*)" # admittedly slightly hacky
-
-    table_name, col_name, updater, where, primary_key = re.search(sql_regex, sql).groups()
-
-    process = subprocess.Popen(
+def popen(updater):
+    return subprocess.Popen(
         ["bb", "-I", "-O", "--stream", "-e", updater],
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
@@ -22,22 +17,30 @@ def process_rows(sql, cursor):
         bufsize=1,  # Line-buffered for real-time streaming
     )
 
+def run_update(sql, cursor):
+
+    sql_regex = r"updateclj\s+(\w+)\s+set\s+(\w+)\s+=(.*?)(where.*?)use primary key(.*)" # admittedly slightly hacky
+
+    table_name, col_name, updater, where, primary_key = re.search(sql_regex, sql).groups()
+
+    process = popen(updater)
+
     try:
 
         select = f'select {primary_key}, {col_name} from {table_name} {where}'
+        update = f'update {table_name} set {col_name} = ? where {primary_key} = ?'
+
         results = cursor.execute(select).fetchall()
 
         for k, edn1 in results:
             process.stdin.write(edn1 + '\n')
             process.stdin.flush()
             edn2 = process.stdout.readline().strip()
+            cursor.execute(update, (edn2, k))
 
-            cursor.execute(
-                f'update {table_name} set {col_name} = ? where {primary_key} = ?',
-                (edn2, k)
-            )
+        return len(results)
 
-#updateclj company set details = (assoc *input* :hi :there) where company_id = 9 use primary key company_id
+        # updateclj company set details = (assoc *input* :hi 3) where company_id > 7 use primary key company_id
 
     finally:
 
@@ -48,13 +51,27 @@ def process_rows(sql, cursor):
 class Cursor:
     def __init__(self, cursor):
         self._cursor = cursor
+        self._rowcount = None
+        self._results = None
 
     def execute(self, sql, params=()):
         sql2 = sql.lstrip().lower()
         if sql2.startswith("updateclj"):
-            return process_rows(sql2, self._cursor)
+            self._rowcount = run_update(sql2, self._cursor)
+            return
 
-        return self._cursor.execute(sql, params)
+        self._cursor.execute(sql, params)
+
+    @property
+    def rowcount(self):
+        if self._rowcount is not None:
+            return self._rowcount
+        return self._cursor.rowcount
+
+    def fetchall(self):
+        if self._results is not None:
+            return self._results
+        return self._cursor.fetchall()
 
     def __getattr__(self, name):
         return getattr(self._cursor, name)
